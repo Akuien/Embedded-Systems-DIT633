f2fbf6cb1400bc0c98267b8a862beb25e4cff456
Anton Golubenko <anton.golubenko@gmail.com>
2023-01-31 12:52:10 +0100

    ex5 finished.

--- src/wp1/e5.c
+++ src/wp1/e5.c
@@ -2,11 +2,10 @@
 // Work package 1 
 // Exercise 5 
 // Submission code: XXXXXXX (provided by your TA-s)
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include <stdbool.h>
+#include <stdio.h> // has functions for standard input and output.
+#include <stdlib.h> // used for implementing random number generator.
+#include <time.h> // used for generating random numbers on each compilation of the program.
+#include <stdbool.h> // used for implementing the boolean variable. 
  
 #define MAX 100 // Defines the maximum number of the values in the table 
 #define MAXNUMBER 20 // Defines the maximum value of random numbers 
@@ -16,63 +15,71 @@
  
 // This function generates a set of random numbers 
 // and fills the table *tab with these numbers 
-void create_random(int *tab) {
-    int i;
-    srand(time(0));
-    for (i = 0; i < MAX; i++) {
-        tab[i] = rand() % MAXNUMBER;
-    }
-};  
+void create_random(int *tab) { // the function takes tab array as an argument.
+    int i; // declaring a variable "i", which is the counter for the for loop below.
+    srand(time(0)); // generator of random numbers.
+    for (i = 0; i < MAX; i++) { // the for loop loops with numbers 0 through 100.
+        tab[i] = rand() % MAXNUMBER; // for every index until 100, the rand() function generates a random number that can be 20 as a max value. 
+    } // end for loop.
+};  // end function
 
  
 // This function takes the *tab of random numbers 
 // and creates a table with the frequency counts for these numbers 
-void count_frequency(int *tab, int *freq ) {
+void count_frequency(int *tab, int *freq ) { // the function takes a tab array of type int, and a frequency array of type int. 
 
-    int i;
+    int i; // the declaration of counter variable. 
     
-    for (i = 0; i < MAX; i++) { 
-        freq[tab[i]]++;
+    for (i = 0; i < MAX; i++) {  // the for loop loops until it reaches a 100
+        freq[tab[i]]++; // this counts all the duplicate indecies and adds that count to the frequency array.
         
-    }
+    } // end of for loop.
 
-};     
+}; // end of function. 
  
 // This function takes the frequency count table 
 // and draws a histogram of the values in that frequency table 
-void draw_histogram(int *freq) {
+void draw_histogram(int *freq) { // the function takes frequency array as an argument. 
 
-    int i, j;
-    bool isFrequencyZero;
+    int i, j; // the counters i and j are declared, i for the outer loop 
+    bool isFrequencyZero; // the bool variable takes true or false, if it is true, then it indicates that it found a unique number in frequency array.
 
-    for (i = 0; i < MAXNUMBER; i++)
+    for (i = 0; i < MAXNUMBER; i++) // the for loop loops until it reaches 20 index numbers.
     {
-        if (freq[i] == 0)
+        // ----------------------------------------------------------------------------------------------------------------------
+
+        if (freq[i] == 0) // if if current position of a number in freq[] array is zero, then that would mean that the number is not duplicated.
         {
-            continue;
-            isFrequencyZero = true;
+            continue; // then skip over that index and dont print it.
+            isFrequencyZero = true; // frequency is then 0. 
         }
         else
         {
-            isFrequencyZero = false;
+            isFrequencyZero = false; // if the number is duplicated then isFrequencyZero set to false.
         }
-        if (i < 10)
+
+        // ----------------------------------------------------------------------------------------------------------------------
+
+        if (i < 10) // if the numbers printed on the left column of the histogram are less than 10.
         {
-            printf("%d    ", i);
+            printf("%d    ", i); // then print a bigger space between numbers and x's. This is due to the fact that numbers under 10 have one digits, therefore require more space.
         }
         else
         {
-            printf("%d   ", i);
+            printf("%d   ", i); // if the numbers are more than 9, then add a lesser space. 
         }
-        for (j = 0; j < freq[i]; j++)
+
+        // -----------------------------------------------------------------------------------------------------------------------
+
+        for (j = 0; j < freq[i]; j++) // the inner loop that loops through the frequeny numbers at each index of the freq[] array. 
         {
-            if (!isFrequencyZero)
+            if (!isFrequencyZero) // if there is frequency greater than 0.
             {
-                printf("%c", 'x');
+                printf("%c", 'x'); // then print out x's.
             }
         }
 
-        printf("\n");
+        printf("\n"); // the divider between the columns of numbers and x's.
     }
 };  
  
@@ -86,14 +93,14 @@ void draw_histogram(int *freq) {
 // If you choose to go for the optional part 
 // Please modify it accordingly
 
-int main(int argc, char const *argv[])
+int main(void) // the main function takes void as an argument.
 {
-    int table[MAX], n; 
-    int frequency[MAXNUMBER] = {0};
+    int table[MAX]; // the table array with the size of max 100 positions is declared.
+    int frequency[MAXNUMBER] = {0}; // the frequency array with the size of max 20 positions is declared and initialized with a 0.
 
-    create_random(table);
-    count_frequency(table, frequency);
-    draw_histogram(frequency);
+    create_random(table); // the function takes the table variable as an argument, and calculates and adds random numbers to that array.
+    count_frequency(table, frequency); // the function takes table and frequency array, and adds the frequency count to frequency array for each table value.
+    draw_histogram(frequency); // the function draws the histogram with indecies on the left side and frequency x's on the right column. 
 
-    return 0;
+    return 0; // return succesffuly. 
 }
