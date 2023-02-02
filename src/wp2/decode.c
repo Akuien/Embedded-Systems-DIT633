da07da9cd0536e807dc6df0fc58ee2ee6de1f240
Aditya Khadkikar <guskhadad@student.gu.se>
2023-02-02 04:57:27 +0100

    e4-dec2hec conv works, now table representation left

--- src/wp2/decode.c
+++ src/wp2/decode.c
@@ -1,117 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char const *argv[])
 {
-    if (argc != 2) {
-        printf("Error: Please enter 1 argument only.");
+    unsigned char byte = 0;
+
+    if (argc != 2 || strlen(argv[1]) != 2) {
+        printf("Error: Please enter 1 argument only, that is strictly a 2-digit hexadecimal value.\n");
         return 1;
     }
 
-    unsigned char byte = 0;
+    if ((!((argv[1])[0] >= 48 && (argv[1])[0] <= 57) && !((argv[1])[0] >= 65 && (argv[1])[0] <= 70)) 
+    || (!((argv[1])[1] >= 48 && (argv[1])[1] <= 57) && !((argv[1])[1] >= 65 && (argv[1])[1] <= 70))) {
+        printf("Error: Not a valid hexadecimal value. Please enter either a digit (0-9) or an UPPERCASE alphabet between A-F.\n");
+        return 1;
+    } else {
+        for (int i=48; i<=57; i++) {
+            if (argv[1][0] == i) {
+                char i_char = i;
+                byte += atoi(&i_char) << 4;
+            }
+            if (argv[1][1] == i) {
+                char i_char = i;
+                byte += atoi(&i_char);
+            }
+        }
 
-    switch(argv[1][0]) {
-        case 49:
-            byte += 1 << 4; 
-            break;
-        case 50:
-            byte += 2 << 4; 
-            break;
-        case 51:
-            byte += 3 << 4; 
-            break;
-        case 52:
-            byte += 4 << 4; 
-            break;
-        case 53:
-            byte += 5 << 4; 
-            break;
-        case 54:
-            byte += 6 << 4;
-            break;
-        case 55:
-            byte += 7 << 4;
-            break;
-        case 56:
-            byte += 8 << 4;
-            break;
-        case 57:
-            byte += 9 << 4;
-            break;
-        case 65:
-            byte += 10 << 4;
-            break;
-        case 66:
-            byte += 11 << 4;
-            break;
-        case 67:
-            byte += 12 << 4;
-            break;
-        case 68:
-            byte += 13 << 4;
-            break;
-        case 69:
-            byte += 14 << 4;
-            break;
-        case 70:
-            byte += 15 << 4;
-            break;
-        default:
-            printf("The entered argument is not a valid hexadecimal.\n");
-            return 1;
+        for (int i=65; i<=70; i++) {
+            if (argv[1][0] == i) {
+                byte += (i-55) << 4;
+            }
+            if (argv[1][1] == i) {
+                byte += (i-55);
+            }
+        }
     }
 
-    switch(argv[1][1]) {
-        case 49:
-            byte += 1; 
-            break;
-        case 50:
-            byte += 2; 
-            break;
-        case 51:
-            byte += 3; 
-            break;
-        case 52:
-            byte += 4; 
-            break;
-        case 53:
-            byte += 5; 
-            break;
-        case 54:
-            byte += 6;
-            break;
-        case 55:
-            byte += 7;
-            break;
-        case 56:
-            byte += 8;
-            break;
-        case 57:
-            byte += 9;
-            break;
-        case 65:
-            byte += 10;
-            break;
-        case 66:
-            byte += 11;
-            break;
-        case 67:
-            byte += 12;
-            break;
-        case 68:
-            byte += 13;
-            break;
-        case 69:
-            byte += 14;
-            break;
-        case 70:
-            byte += 15;
-            break;
-        default:
-            printf("The entered argument is not a valid hexadecimal.\n");
-            return 1;
-    }
-
-    printf("%d\n", byte);
+    byte < 10 ? printf("0%d\n", byte) : printf("%d\n", byte);
     return 0;
 }
