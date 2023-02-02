8688a9681b3fd37af97faf1226f952c296f0060b
Anton Golubenko <anton.golubenko@gmail.com>
2023-02-02 19:32:04 +0100

    e2 done with comments

--- src/wp2/e2.c
+++ src/wp2/e2.c
@@ -3,57 +3,54 @@
 // Exercise 1 
 // Submission code: XXXXXX (provided by your TA-s) 
 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <time.h> 
+#include <stdio.h> // used for standard output and input
+#include <stdlib.h> // used for random number generation function.
+#include <time.h> // used for generating random seed.
  
 //#### Constants ##### 
-#define MAX 5   
+#define MAX 5 // maximum size if 5
  
 // ##### typedefs          #### 
-typedef struct q{  
-        int number; 
-        struct q *next; 
-        struct q *prev; 
+typedef struct q{ // is a type struct, that creates an object. 
+    int number; // number attribute of the struct q, stores a number value of the node.
+    struct q *next; // is a pointer to the node after current node.
+    struct q *prev; // is a pointer to the node before current node.
 } REGTYPE; 
  
 // ##### Function declarations   ##### 
  
-REGTYPE* random_list(void); 
-REGTYPE* add_first(REGTYPE* temp, int data); 
+REGTYPE* random_list(void); // functions that generates a linked list with random values.
+REGTYPE* add_first(REGTYPE* temp, int data); // adds value entered by a user into the first position of the linked list.
  
  
 //###### Main program ####### 
 int main(int argc, char *argv[]) 
-{ 
-    int nr=0, user_input;
+{
+    int nr = 0, user_input; // integer nr is declared and initialized, it stands for the counter of number of records; user_input is used for storing input from a user in integer form.
 
+    REGTYPE *act_post , *head=NULL; // we create act_post and head values which both will store the first node of the linked list
 
-    REGTYPE *act_post , *head=NULL;
-    
     srand(time(0)); // Random seed
-    head=random_list(); 
+    
+    head=random_list(); // assign the first node of linked list to head value.
 
-    act_post=head; 
-    while( act_post!=NULL){ 
+    act_post=head; // assign act_post to head.
+    while( act_post!=NULL){  // while the node is not pointing at NULL
 
-        printf("\n Post nr %d : %d", nr++, act_post->number); 
-        act_post=act_post->next; 
+        printf("\n Post nr %d : %d", nr++, act_post->number); // print record number, and the random generated number.
+        act_post=act_post->next; // keep pointing at the next node until NULL.
     }
 
-    printf("\nEnter a new record: ");
-    scanf("%d", &user_input);
+    printf("\nEnter a new record: "); // print a statement which prompts the user to enter a new record.
+    scanf("%d", &user_input); // take that input value from the user, and store it in user_input variable.
 
-    head = add_first(head, user_input);
-    nr = 0;
-    while( head !=NULL){ 
+    head = add_first(head, user_input); // store the new record in head value.
+    nr = 0; // set nr to 0 again.
+    while( head !=NULL){  // while the node is not pointing at NULL
 
-    printf("\n Post nr %d : %d", nr++, head->number);  
-    head=head->next; 
+    printf("\n Post nr %d : %d", nr++, head->number);  // print record numbers and random generated numbers next to them.
+    head=head->next; // keep pointing at next node until it points at NULL
     }
-
-
-    add_first(head, user_input);
 
 
     // --- Free the allocated memory  ---
@@ -72,58 +69,41 @@ int main(int argc, char *argv[])
 // ====     End of main   ====================================== 
  
 REGTYPE* random_list(void){ 
-    int nr, i=0; 
-    REGTYPE *top, *old, *item, *n1 = NULL, *n2 = NULL, *n3 = NULL, *n4 = NULL;
-    
+    int nr, i=0; // integers nr and i are declared, where i is the counter of a for loop.
+    REGTYPE *top, *old, *item, *n1 = NULL, *n2 = NULL, *n3 = NULL, *n4 = NULL; // declare REGTYPE variables and set them to NULL.
+    // lines 75 through 79 are memory allocation for new nodes in the linkedlist, top is the first element in the linkedlist and n4 is the last element.
     top = malloc(sizeof(struct q));
-    // item = malloc(sizeof(struct q));
-    // old = malloc(sizeof(struct q));
     n1 = malloc(sizeof(struct q));
     n2 = malloc(sizeof(struct q));
     n3 = malloc(sizeof(struct q)); 
     n4 = malloc(sizeof(struct q)); 
-    srand(time(0));
+    srand(time(0)); // random seed generator to generate random numbers.
 
     
-    
-    // for (i; i < MAX; i++)
-    // {
-    //     top = item;
-    //     if(i <= 4) {
-    //         item->number = rand() % 100;
-    //     }
-    //     item = item->next;
+    top->number = rand() % 100;  // the number attribute of top gets a random number.
+    n1->number = rand() % 100;  // the number attribute of n1 gets a random number.
+    n2->number = rand() % 100;  // the number attribute of n2 gets a random number.
+    n3->number = rand() % 100;  // the number attribute of n3 gets a random number.
+    n4->number = rand() % 100; // the number attribute of n4 gets a random number.
 
-    //     if(i == 4) {
-    //         item->next = NULL;
-    //     }
-        
-    // }
+    top->next = n1; // pointer of top is assigned to n1 variable.
+    n1->next = n2; // pointer of n1 is assigned to n2 variable.
+    n2->next = n3; // pointer of n2 is assigned to n3 variable.
+    n3->next = n4; // pointer of n3 is assigned to n4 variable.
+    n4->next = NULL; // pointer of n4 is assigned to NULL to indicate the end of linked list.
 
-    top->number = rand() % 100; 
-    n1->number = rand() % 100;  
-    n2->number = rand() % 100;  
-    n3->number = rand() % 100;  
-    n4->number = rand() % 100; 
-
-    top->next = n1;
-    n1->next = n2;
-    n2->next = n3;
-    n3->next = n4;
-    n4->next = NULL;
-
-   return (top); 
+    return (top); // return the first element of the linked list.
 } 
  
 //==========================================================                 
  
 REGTYPE* add_first(REGTYPE* temp, int data){
 
-    struct q* new_rec = (struct q*) malloc(sizeof(struct q));
+    struct q* new_rec = (struct q*) malloc(sizeof(struct q)); // declaring and initializing new variable new_rec of type struct q, which will be the new record.
 
-    new_rec->number = data;
+    new_rec->number = data; // assign the number of new_rec to data, which is a number entered by a user.
 
-    new_rec->next = temp;
-    temp = new_rec;
-    return temp;
+    new_rec->next = temp; // new_rec will be pointing at previous top node of the linkedlist.
+    temp = new_rec; // the new top node of the linked list is assigned to new_rec. 
+    return temp; // return the new top node of the linked list.
 }
