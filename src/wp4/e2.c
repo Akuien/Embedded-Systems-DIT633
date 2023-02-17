e3086b4b62c13e19555c04a07a977221147983d3
akuien <akuien@student.chalmers.se>
2023-02-17 12:30:45 +0100

    e2 fully commented

--- src/wp4/e2.c
+++ src/wp4/e2.c
@@ -1,7 +1,7 @@
 // C++ code
 // (C) Anton Golubenko, Aditya Khadkikar, Akuien Deng - Group: 27 
 // Work package 4
-// Exercise 1 
+// Exercise 2 
 // Submission code: AW1Y2XM (provided by your TA-s) 
 
 //include the servo(motor) library
@@ -23,11 +23,11 @@ int current_seconds = 0;     // Initialize the time passed to 0
 
 void setup()
 {
-    //Set the timer
+    //Set the timer to timer0
   	//set the CTC mode
     TCCR0A = (1<< WGM01);
   	
-  	//value for ORC0A for ms
+  	//value for ORC0A for microseconds
     OCR0A = 0xF9;
   
     // set the interrupt request
@@ -35,13 +35,9 @@ void setup()
     sei(); // enable interrupt
   
   
-    TCCR0B |= (1<<CS01); //Set the prescale 1/64 clock
+    TCCR0B |= (1<<CS01); //Set the prescale
     TCCR0B |= (1<<CS00);
     
-    
-    // Start the serial port
-    Serial.begin(9600);
-  	
   
     seconds_servo.attach(10, MIN_DEGREE, MAX_DEGREE); // Set the motor to pin 12, while setting the min and max degrees to 0 to 180 respectively.
     seconds_servo.write(0); //set the motor to 0(zero)
@@ -50,11 +46,13 @@ void setup()
 
 //Move the motor
 void rotate_servo(){
-    current_degrees_rotated = (current_degrees_rotated + DEGREE_TIME_MAPPING) % (MAX_DEGREE + 1); // Increment the rotation degrees and modulou it so if current_degrees is > 180, it is set to 0. Incremenet UPPER_DEGREE (180) by 1 so when mDegrees is 180 it does not get set to 0.
+  	 // Increment the rotation degrees and modulou it so if current_degrees is > 180, it is set to 0.
+    //Incremenet UPPER_DEGREE (180) by 1 so when mDegrees is 180 it does not get set to 0.
+    current_degrees_rotated = (current_degrees_rotated + DEGREE_TIME_MAPPING) % (MAX_DEGREE + 1);
     seconds_servo.write(current_degrees_rotated); 
     }
 
-
+// The interrupt service routine (ISR) that is a set of commands run when the program is interrupted.
 ISR(TIMER0_COMPA_vect)
 {                          
     current_seconds++;    // Increment the time
